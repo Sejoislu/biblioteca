528e6d92342debc75cca30b692e55d3c7eb1cd3c
Jose luis <sejoislu21@gmail.com>
2018-09-15 08:53:42 -0500

    Actualizacion del 15092018 primer commit

---
+++ blog/app/Http/Controllers/Ejemplocontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Ejemplocontroller extends Controller
+{
+	public function ejemplo(){
+		return "Esto es un Ejemplo de controlador en laravel";
+	}
+}
---
+++ blog/resources/views/Libros/NuevoLibro.blade.php
@@ -0,0 +1,10 @@
+@extends('plantillas.admin')
+
+@section('titulo','Nuevo Libro')
+
+@section('contenido')
+ 
+<h2>Nuevo Libro</h2>
+
+
+@endsection
